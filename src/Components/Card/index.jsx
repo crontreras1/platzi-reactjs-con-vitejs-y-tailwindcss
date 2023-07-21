f3ad5a070d9c6d25d8edd2be5b312f225dc35385
cristian contreras <cr.ontreras@gmail.com>
2023-07-21 14:36:29 -0500

    add and unadd product from detail section. Done

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,8 +5,16 @@ import { ShoppingCartContext } from "../../Context";
 function Card (data) {
     const context = useContext(ShoppingCartContext); 
 
+    const showProduct = (productOnDetail) => {
+        context.openProductDetail();
+        context.setProductToShow(productOnDetail);
+    }
+
     return (
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+        <div
+            className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+            onClick={ () => showProduct(data.data) }
+        >
             <figure className="relative mb-3 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{ data.data.category.name }</span>
 
@@ -14,7 +22,7 @@ function Card (data) {
 
                 <div 
                     className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={() => context.setCount(context.count + 1)}    
+                    onClick={ () => context.setCount(context.count + 1) }    
                 >
                     <PlusIcon className="h-6 w-6 text-black" />
                 </div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,12 +3,26 @@ import { createContext, useState } from "react";
 const ShoppingCartContext = createContext();
 
 const ShoppingCartProvider = ({ children }) => {
+    // Cart Icon - Increment cuantity
     const [count, setCount] = useState(0); 
+
+    // Product Detail - Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false); 
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Show Products on detail section
+    const [productToShow, setProductToShow] = useState({});
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow, 
+            setProductToShow
         }}>
             { children }
         </ShoppingCartContext.Provider>
